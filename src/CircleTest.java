dcb4aed9be54a432600253c8b6052382913ff175
boomka <vinh99657@gmail.com>
2022-08-11 14:33:39 +0700

    tuan2

---
+++ src/CircleTest.java
@@ -0,0 +1,7 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle test = new Circle(2,"red");
+
+        System.out.println(test);
+    }
+}
---
+++ src/CylinderTest.java
@@ -0,0 +1,7 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder test2 = new Cylinder(10,10,"green");
+        System.out.println(test2);
+
+    }
+}
